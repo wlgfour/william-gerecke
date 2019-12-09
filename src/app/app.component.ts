0fa765994cb379ffc7f501f215fa920825ca36c0
Will Gerecke <wlgfour@gmail.com>
2019-12-09 00:14:03 -0500

    made nav bar better

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,7 @@ import anime from 'animejs/lib/anime.es.js';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit, AfterViewInit {
-  menuStatus = true;
-  menuAnimation = {
-    play: () => null,
-    reverse: () => null,
-  };
+  menuStatus = false;
 
   constructor() {
   }
@@ -20,23 +16,9 @@ export class AppComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit() {
-    const ani = anime({
-      targets: 'nav li',
-      height:  '0',
-      padding: '0',
-      delay: (el, i) => Math.sqrt(i) * i * 100,
-      duration: 500,
-      autoplay: false,
-      easing: 'easeInOutQuad'
-    });
-    this.menuAnimation.play = ani.play;
-    this.menuAnimation.reverse = ani.reverse;
-    this.menuAnimation.reverse();
   }
 
   menuClick() {
     this.menuStatus = !this.menuStatus;
-    this.menuAnimation.reverse();
-    this.menuAnimation.play();
   }
 }
